09e181bdb1c8cfacecb08b5ac4967170c8e28b4b
Alexander Perez <alexander.pereze02@gmail.com>
2023-11-29 17:07:42 -0500

    score management and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
+import { useState } from 'react';
 import QUESTIONS from './questions.json'
 
+QUESTIONS.sort(() => Math.random() - 0.5) // randomize questions
+
 function App() {
-  return <main>
-    <h1>Quiz</h1>
-    <ul>
-      {QUESTIONS.map((question) => (
-        <li key={question.id}>
-          <h2>{question.question}</h2>
-          <ul>
-            {question.answers.map((answer) => (
-              <li key={answer.id}>
-                <label>
-                  <input type="radio" name={question.id} value={answer.id} />
-                  {answer.text}
-                </label>
-              </li>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
-  </main>;
+  const [index, setIndex] = useState(0) // current question index
+  const [score, setScore] = useState(0) // current score
+  const question = QUESTIONS[index] // get current question
+  const isFinished = index >= QUESTIONS.length // check if quiz is finished
+
+  const handleAnswer = (answer) => {
+    if (answer.isCorrect) {
+      setScore(score + 1)
+    }
+    setIndex(index + 1)
+  }
+
+  return (
+    <main className='grid place-items-center min-h-screen'>
+      <article className='flex flex-col gap-6 text-center bg-teal-700 p-4 rounded max-w-md w-full'>
+        {
+          isFinished ? (<h2>Finished!</h2>) : (
+            <>
+              <h2 className='text-lg'>{question.question}</h2>
+              <ul className='flex flex-col gap-2'>
+                {question.answers.map((answer) => (
+                  <li key={answer.text}>
+                    <label>
+                      <button className='bg-teal-400 p-2 w-full' onClick={() => handleAnswer(answer)}>{answer.text}</button>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )
+        }
+      </article>
+    </main>
+  );
 }
 
 export default App
