d35a7463a68a2452d245fb55454ad5e88e32c3b9
Alexander Perez <alexander.pereze02@gmail.com>
2023-11-29 16:45:08 -0500

    display questions on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import './App.css'
+import QUESTIONS from './questions.json'
 
 function App() {
-  return <main></main>;
+  return <main>
+    <h1>Quiz</h1>
+    <ul>
+      {QUESTIONS.map((question) => (
+        <li key={question.id}>
+          <h2>{question.question}</h2>
+          <ul>
+            {question.answers.map((answer) => (
+              <li key={answer.id}>
+                <label>
+                  <input type="radio" name={question.id} value={answer.id} />
+                  {answer.text}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  </main>;
 }
 
 export default App
